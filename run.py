91c7674a391e639782a1f71a239030a54aeec9d4
alanmac17 <alan17wilson@gmail.com>
2022-06-07 22:11:17 +0000

    confirm access to google sheets

--- run.py
+++ run.py
@@ -11,3 +11,7 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('the_hub')
+
+employees = SHEET.worksheet('employees')
+employee_data = employees.get_all_values()
+print(employee_data)
