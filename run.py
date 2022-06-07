cc8cfa1a50b65251b772fd1558636d640d2897ef
alanmac17 <alan17wilson@gmail.com>
2022-06-07 22:07:23 +0000

    install gspread, update scope

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
 import gspread
 from google.oauth2.service_account import Credentials
 
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('the_hub')
