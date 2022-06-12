06f2bf22b4cd2d3e045f624f79e2ef5c3368aa96
alanmac17 <alan17wilson@gmail.com>
2022-06-12 19:30:44 +0000

    move gspread import data to beginning of records.py and add import call for records file at beginning of run.py

--- records.py
+++ records.py
@@ -1,29 +1,28 @@
-class EmployeeProfile:
+import gspread
+from google.oauth2.service_account import Credentials
+import pandas as pd
 
-    def __init__(self, employee_id: str):
-        import gspread
-        from google.oauth2.service_account import Credentials
-        import pandas as pd
 
-        SCOPE = [
+SCOPE = [
             "https://www.googleapis.com/auth/spreadsheets",
             "https://www.googleapis.com/auth/drive.file",
             "https://www.googleapis.com/auth/drive"
             ]
 
-        CREDS = Credentials.from_service_account_file('creds.json')
-        SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-        GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-        SHEET = GSPREAD_CLIENT.open('the_hub')
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('the_hub')
 
-        database = SHEET.worksheet('employees')
-        db = pd.DataFrame(database.get_all_records())
-        db.set_index('code', inplace=True)
-        print(db.head())
+database = SHEET.worksheet('employees')
+db = pd.DataFrame(database.get_all_records())
+db.set_index('code', inplace=True)
 
+class EmployeeProfile:
+
+    def __init__(self, employee_id: str):
         employee = db.loc[employee_id]
 
-        self.employee_id = employee.code
         self.first_name = employee.first_name
         self.last_name = employee.last_name
         self.email = employee.email_address
@@ -38,7 +37,5 @@ class EmployeeProfile:
         self.six_month_eval_email = employee.six_month_eval_email
         self.pass_probation = employee.pass_probation
 
-        employee = EmployeeProfile(employee_id)
-
-        def get_profile(self):
-            print(self.first_name)
+    def get_profile(self):
+        print(self.first_name)
--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-import employee
+import records
 
 def main():
 
@@ -8,8 +8,8 @@ def main():
     print("Please enter an employee ID")
     print("Example: 123\n")
 
-    employee_id = (input("Enter Employee ID here: \n"))
-        
+    employee_id = (int(input("Enter Employee ID here: \n")))
+    
     print("1 to View Employee Profile")
     print("2 to View Entitlements")
     print("3 to Send Onboarding Email")
@@ -17,68 +17,9 @@ def main():
     print("5 to Quit")
 
     selection = int((input("Select an option: ")))
-   
+    
     if selection == 1:
-        get_profile(employee_id)
-    elif selection == 2:
-        get_entitlements()
-    elif selection == 3:
-        send_email()
-    elif selection == 4:
-        send_email()
-    elif selection == 5:
-        sys.exit(0)
-    else:
-        print("Please select a number from the options listed")
-        main()
+        records.EmployeeProfile(employee_id).get_profile()
 
 print("Welcome to the Hub")
 main()
-
-
-# def get_profile():
-   
-#     employee_id = db.loc[Id]
-
-#     print(f"Name: {employee_id.first_name} {employee_id.last_name}")
-#     print(f"Role: {employee_id.role}")
-#     print(f"Manager: {employee_id.line_manager}")
-#     print(f"Email: {employee_id.email_address}")
-#     print(f"Status: {employee_id.employee_type}")
-
-# def get_entitlements():
-#     """
-#     Work out employee benefits based on role, time
-#     """
-#     print("Please enter an employee ID")
-#     print("Example: 123\n")
-
-#     Id = int(input("Enter Employee ID here: \n"))
-
-#     def sabatical():
-#     ##employees are entitled to 1 month sabatical every 4 years worked
-#         employee_id = db.loc[Id]
-        
-# def employee_next_action()
-# """
-# Work out the next appropriate action for employee
-# """
-# def get_training_material()
-# """
-# linkedin learning API and retrive training course data based on role
-# """
-
-# def get_latest_social_feeds()
-# """
-# Access company social sites and retrive latest posts
-# """
-
-# def format_onboarding_email()
-# """
-# format onboarding email with content from benefits, actions, material, social feeds
-# """
-
-# def send_email()
-# """
-# send email to employee and hiring manager
-# """
